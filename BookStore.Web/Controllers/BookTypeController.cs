2f566b112789dc09bb7c97d13629dc1f14d31217
minaozk <80757513+minaozk@users.noreply.github.com>
2023-10-30 16:06:24 +0300

    Repository Pattern has been created.

--- BookStore.Web/Controllers/BookTypeController.cs
+++ BookStore.Web/Controllers/BookTypeController.cs
@@ -30,9 +30,69 @@ namespace BookStore.Web.Controllers
 			if (ModelState.IsValid)
 			{
 				_dbContext.BookTypes.Add(bookType);
-			_dbContext.SaveChanges();
+				_dbContext.SaveChanges();
+				TempData["success"] = "Kitap türü başarıyla eklendi";
+
 			return RedirectToAction("Index", "BookType");
 			}
+
+			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			BookType? bookTypeDb = _dbContext.BookTypes.Find(id);
+			if (bookTypeDb == null)
+			{
+				return NotFound();
+			}
+			return View(bookTypeDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(BookType bookType)
+		{
+			if (ModelState.IsValid)
+			{
+				_dbContext.BookTypes.Update(bookType);
+				_dbContext.SaveChanges();
+				TempData["success"] = "Kitap türü başarıyla güncellendi.";
+				return RedirectToAction("Index", "BookType");
+			}
+
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			BookType? bookTypeDb = _dbContext.BookTypes.Find(id);
+			if (bookTypeDb == null)
+			{
+				return NotFound();
+			}
+			return View(bookTypeDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			BookType? bookType = _dbContext.BookTypes.Find(id);
+			if (bookType == null)
+			{
+				return NotFound();
+			}
+
+			_dbContext.BookTypes.Remove(bookType);
+			_dbContext.SaveChanges();
+			TempData["success"] = "Kitap türü başarıyla silindi.";
+			return RedirectToAction("Index", "BookType");
 
 			return View();
 		}
---
+++ BookStore.Web/Models/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using BookStore.Web.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Web.Models
+{
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		private readonly ApplicationDbContext _applicationDbContext;
+		internal DbSet<T> dbSet;
+
+		public Repository(ApplicationDbContext applicationDbContext)
+		{
+			_applicationDbContext = applicationDbContext;
+			this.dbSet = _applicationDbContext.Set<T>();
+		}
+
+		public IEnumerable<T> GetAll()
+		{
+			IQueryable<T> query = dbSet;
+			return query.ToList();
+		}
+
+		public T Get(Expression<Func<T, bool>> filter)
+		{
+			IQueryable<T> query = dbSet;
+			query = query.Where(filter);
+			return query.FirstOrDefault();
+		}
+
+		public void Add(T entity)
+		{
+			dbSet.Add(entity);
+		}
+
+		public void Remove(T entity)
+		{
+			dbSet.Remove(entity);
+		}
+
+		public void DeleteRange(IEnumerable<T> entities)
+		{
+			dbSet.RemoveRange(entities);
+		}
+	}
+}
