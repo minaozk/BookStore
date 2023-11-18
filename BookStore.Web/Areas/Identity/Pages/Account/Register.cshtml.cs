642709788e87d46e54eb01c8a873f23dba93da5f
minaozk <80757513+minaozk@users.noreply.github.com>
2023-11-18 23:28:01 +0300

    Adding Custom ApplicationUser Information to the Register Razor Page

--- BookStore.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -87,7 +87,7 @@ namespace BookStore.Web.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Şifre")]
             public string Password { get; set; }
 
             /// <summary>
